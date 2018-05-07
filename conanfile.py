5824cf53f3fbf166a16b630e3ed15b9d3b7a8a14
Uilian Ries <uilianries@gmail.com>
2018-05-07 20:39:54 -0300

    #245 Copy project file
    - Copy LICENSE file to package
    - Avoid to copy source folder to build (header-only)
    - Use full example on package test
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- conanfile.py
+++ conanfile.py
@@ -9,19 +9,21 @@ class GreatestConan(ConanFile):
     name = "greatest"
     version = "1.3.1"
     url = "https://github.com/bincrafters/conan-greatest"
+    homepage = "https://github.com/silentbicycle/greatest"
+    author = "Bincrafters <bincrafters@gmail.com>"
     description = "A C testing library in 1 file. No dependencies, no dynamic allocation. "
     license = "ISC"
     exports = ["LICENSE.md"]
     source_subfolder = "source_subfolder"
-    
+    no_copy_source=True
+
     def source(self):
-        source_url = "https://github.com/silentbicycle/greatest"
-        tools.get("{0}/archive/v{1}.tar.gz".format(source_url, self.version))
+        tools.get("{0}/archive/v{1}.tar.gz".format(self.homepage, self.version))
         extracted_dir = self.name + "-" + self.version
         os.rename(extracted_dir, self.source_subfolder)
 
     def package(self):
-        self.copy(pattern="LICENSE", dst="licenses")
+        self.copy(pattern="LICENSE", dst="licenses", src=self.source_subfolder)
         self.copy(pattern="greatest.h", dst="include", src=self.source_subfolder)
 
     def package_id(self):
